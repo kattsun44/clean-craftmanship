adddec6979cc404caecac7b01dd7a9b2a936b0cf
kattsun44 <kattsun4416@gmail.com>
2023-05-13 23:48:20 +0900

    p.55

---
+++ src/test/java/com/example/StackTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Stack.
+ */
+public class StackTest {
+    @Test
+    public void canCreateStack() throws Exception {
+        MyStack stack = new MyStack();
+    }
+}
