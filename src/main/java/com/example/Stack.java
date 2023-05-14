b542cb6c74674d1795ecddaa1babff5b22cc2c3d
kattsun44 <kattsun4416@gmail.com>
2023-05-14 16:50:35 +0900

    [GREEN] p.59

--- src/main/java/com/example/Stack.java
+++ src/main/java/com/example/Stack.java
@@ -1,10 +1,13 @@
 package com.example;
 
 public class Stack {
+    private boolean empty = true;
+
     public boolean isEmpty() {
-        return true;
+        return empty;
     }
 
     public void push(int element) {
+        empty = false;
     }
 }
