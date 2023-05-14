96995f688c097880d68f6a3d4fefe614a55f3b11
kattsun44 <kattsun4416@gmail.com>
2023-05-15 08:31:37 +0900

    ♻️  p.81
    "Refactor..." > "Extract to method" で抽出

--- src/test/java/com/example/BowlingTest.java
+++ src/test/java/com/example/BowlingTest.java
@@ -13,19 +13,21 @@ public class BowlingTest {
         g = new Game();
     }
 
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
+    }
+
     @Test
     public void gatterGame() throws Exception {
-        for (int i = 0; i < 20; i++) {
-            g.roll(0);
-        }
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
 
     @Test
     public void allOnes() throws Exception {
-        for (int i = 0; i < 20; i++) {
-            g.roll(1);
-        }
+        rollMany(20, 1);
         assertEquals(20, g.score());
     }
 }
