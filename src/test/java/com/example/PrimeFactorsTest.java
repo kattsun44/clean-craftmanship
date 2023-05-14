b0eb9f69327b2162a37ddfab6d7e4eae4f0c9408
kattsun44 <kattsun4416@gmail.com>
2023-05-14 19:01:22 +0900

    [refactor] p.74

--- src/test/java/com/example/PrimeFactorsTest.java
+++ src/test/java/com/example/PrimeFactorsTest.java
@@ -25,16 +25,11 @@ public class PrimeFactorsTest {
 
     private List<Integer> factorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        int divisor = 2;
-        while (n > 1) {
-            while (n % divisor == 0) {
+
+        for (int divisor = 2; n > 1; divisor++)
+            for (; n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-                n /= divisor;
-            }
-            divisor++;
-        }
-        if (n > 1)
-            factors.add(n);
+
         return factors;
     }
 }
