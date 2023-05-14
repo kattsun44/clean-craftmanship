35b74023b5de0677aaadf61a898095afa494f30a
kattsun44 <kattsun4416@gmail.com>
2023-05-14 18:48:05 +0900

    [refactor] p.71

--- src/test/java/com/example/PrimeFactorsTest.java
+++ src/test/java/com/example/PrimeFactorsTest.java
@@ -25,9 +25,9 @@ public class PrimeFactorsTest {
                 factors.add(2);
                 n /= 2;
             }
-            if (n > 1)
-                factors.add(n);
         }
+        if (n > 1)
+            factors.add(n);
         return factors;
     }
 }
