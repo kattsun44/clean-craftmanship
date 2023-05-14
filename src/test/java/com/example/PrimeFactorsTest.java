ca39c8cbcf067299cc00ef5b84573280568ab2ec
kattsun44 <kattsun4416@gmail.com>
2023-05-14 18:16:21 +0900

    [red] p.69

--- src/test/java/com/example/PrimeFactorsTest.java
+++ src/test/java/com/example/PrimeFactorsTest.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,6 +13,7 @@ public class PrimeFactorsTest {
     @Test
     public void factors() throws Exception {
         assertThat(factorsOf(1), is(empty()));
+        assertThat(factorsOf(2), contains(2));
     }
 
     private List<Integer> factorsOf(int n) {
