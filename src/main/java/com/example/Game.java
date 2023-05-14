8a113e1188096a1ff613be057055080def96aed4
kattsun44 <kattsun4416@gmail.com>
2023-05-15 08:24:30 +0900

    [green] p.80 allOnes()

--- src/main/java/com/example/Game.java
+++ src/main/java/com/example/Game.java
@@ -1,10 +1,13 @@
 package com.example;
 
 public class Game {
+    private int score;
+
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
